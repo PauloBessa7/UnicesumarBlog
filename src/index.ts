7b3c70579dfbbdd28793de64af91d9eb39002f9e
PauloBessa7 <pauloh.bess@gmail.com>
2024-09-27 21:12:25 -0300

    Arrumando structure

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/users', async function (req: Request, res: Response) {
     try {
         const [rows] = await connection.query(`
-            SELECT u.id, u.name, u.email, u.ativo, p.descricao, DATE_FORMAT(u.created_at, '%d-%m-%Y') AS formatted_date FROM users u
-            INNER JOIN papel p ON u.papel_id = p.id
-            ORDER BY u.id ASC;
+            SELECT 
+                u.id, 
+                u.name, 
+                u.email, 
+                u.ativo, 
+                u.papel AS descricao,
+                DATE_FORMAT(u.created_at, '%d-%m-%Y') AS formatted_date 
+            FROM 
+                users u
+            ORDER BY 
+                u.id ASC;
         `);
 
         return res.render('users/index', {
@@ -45,7 +53,7 @@ app.get('/users/add', async function (req: Request, res: Response) {
 app.post('/users', async function (req: Request, res: Response) {
     const body = req.body;
     const isAtivo = body.ativo ? true : false;
-    const insertQuery = "INSERT INTO users (name, email, senha, ativo, papel_id) VALUES (?, ?, ?, ?, ?)";
+    const insertQuery = "INSERT INTO users (name, email, senha, ativo, papel) VALUES (?, ?, ?, ?, ?)";
 
     if (body.password !== body.confirmPassword) {
         return res.redirect('/users/add');
@@ -60,40 +68,40 @@ app.post('/users', async function (req: Request, res: Response) {
     }
 });
 
-app.post('/users/:id/delete', async function(req : Request, rep : Response){
+app.post('/users/:id/delete', async function (req: Request, rep: Response) {
     const id = req.params.id;
     const sqlDelete = 'Delete from users where id = ?';
-    await connection.query(sqlDelete,[id])
+    await connection.query(sqlDelete, [id])
     rep.redirect("/users")
 })
 
-app.get('/login', async function(req : Request, rep : Response){
-    return rep.render('users/login') 
+app.get('/login', async function (req: Request, rep: Response) {
+    return rep.render('users/login')
 })
 
 
-app.post('/login', async function(req : Request, rep : Response){
+app.post('/login', async function (req: Request, rep: Response) {
     const email = req.body.email;
     const senha = req.body.password;
     const query = 'SELECT * FROM users WHERE email = ? AND senha = ?';
-    
+
     try {
         const [results] = await connection.query(query, [email, senha]);
         console.log(results)
         if (results.length > 0) {
-            return rep.render('users/blog') 
+            return rep.render('users/blog')
         } else {
             console.log('Falha no login');
             // Você pode tratar a falha de login aqui
             return rep.render('users/login')
         }
-      } catch (err) {
-        
-      }
+    } catch (err) {
+
+    }
 })
 
-app.get('/', function(req, resp){
-    return resp.render('users/blog') 
+app.get('/', function (req, resp) {
+    return resp.render('users/blog')
 })
 
 app.listen('3000', () => console.log("Server is listening on port 3000"));
--- structure.sql
+++ structure.sql
@@ -1,48 +1,35 @@
-CREATE TABLE papel (
-  id int,
-  descricao VARCHAR(15),
-  CONSTRAINT id_papel_pk PRIMARY KEY(id)
-)
-
 CREATE TABLE users (
   id INT AUTO_INCREMENT,
   name VARCHAR(30),
   email VARCHAR(50),
   senha VARCHAR(30),  
-  ativo BOOLEAN , 
-  papel_id INT,
+  ativo BOOLEAN, 
+  papel VARCHAR(15), 
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT id_users_pk PRIMARY KEY (id),
-  CONSTRAINT id_papel_fk FOREIGN KEY (papel_id) REFERENCES papel(id)
+  CONSTRAINT id_users_pk PRIMARY KEY (id)
 );
 
 
-INSERT INTO papel(id,descricao)  VALUES (0, 'Usuario');
-INSERT INTO papel(id, descricao) VALUES (1, 'Administrador');
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Paulo Bessa", "paulo.bessa@gmail.com", "Admin", true, 'Administrador');
 
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Rafael Labegalini", "rafael.labegalini@gmail.com", "123Mudar@", true, 'Administrador');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Renata Capoia", "renata.capoia@gmail.com", "Admin", true, 1);
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Maria Silva", "maria.silva@gmail.com", "123Mudar@", true, 'Usuario');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Rafael Labegalini", "rafael.labegalini@gmail.com", "123Mudar@", true, 1);
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("João Souza", "joao.souza@gmail.com", "123Mudar@", true, 'Usuario');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Maria Silva", "maria.silva@gmail.com", "123Mudar@", true, 0);
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Carla Ferreira", "carla.ferreira@gmail.com", "123Mudar@", true, 'Usuario');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("João Souza", "joao.souza@gmail.com", "123Mudar@", true, 0);
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Pedro Oliveira", "pedro.oliveira@gmail.com", "123Mudar@", true, 'Usuario');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Carla Ferreira", "carla.ferreira@gmail.com", "123Mudar@", true, 0);
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Ana Lima", "ana.lima@gmail.com", "123Mudar@", true, 'Usuario');
 
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Pedro Oliveira", "pedro.oliveira@gmail.com", "123Mudar@", true, 0);
-
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Ana Lima", "ana.lima@gmail.com", "123Mudar@", true, 0);
-
-INSERT INTO users(name, email, senha, ativo, papel_id) 
-VALUES ("Lucas Martins", "lucas.martins@gmail.com", "123Mudar@", true, 0);
-
-
+INSERT INTO users(name, email, senha, ativo, papel) 
+VALUES ("Lucas Martins", "lucas.martins@gmail.com", "123Mudar@", true, 'Usuario');
